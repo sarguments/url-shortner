48bd399d2039abeaf0412594d99d96ca3b706b8d
saru <sarguments@gmail.com>
2021-03-22 00:23:36 +0900

    feat: url 생성 요청 시 요청값 검증 추가

--- src/main/java/me/saru/urlshortner/data/ShortUrlDto.java
+++ src/main/java/me/saru/urlshortner/data/ShortUrlDto.java
@@ -2,6 +2,9 @@ package me.saru.urlshortner.data;
 
 import lombok.*;
 
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+
 public class ShortUrlDto {
 
     @Getter
@@ -9,6 +12,8 @@ public class ShortUrlDto {
     @NoArgsConstructor
     @AllArgsConstructor
     public static class Req {
+        @NotEmpty(message = "Url must be not empty")
+        @Size(max = 1000, message = "Url length must be under 1000")
         private String url;
     }
 
--- src/test/java/me/saru/urlshortner/controller/api/ShortUrlControllerTest.java
+++ src/test/java/me/saru/urlshortner/controller/api/ShortUrlControllerTest.java
@@ -6,6 +6,8 @@ import me.saru.urlshortner.domain.ShortUrl;
 import org.junit.jupiter.api.Test;
 import org.springframework.http.MediaType;
 
+import java.util.Collections;
+
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
@@ -33,5 +35,36 @@ class ShortUrlControllerTest extends DefaultSpringTest<ShortUrl> {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.success", is(true)))
                 .andExpect(jsonPath("$.response.url").isNotEmpty());
+    }
+
+    @Test
+    void generateShortUrlTooLong() throws Exception {
+        mockMvc.perform(post("/api/shorturl")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(
+                        new ShortUrlDto.Req(String.join("", Collections.nCopies(100, "https://test.com/asdf")))
+                        )
+                )
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect(jsonPath("$.response").isEmpty())
+                .andExpect(jsonPath("$.error.message", is("Url length must be under 1000")));
+    }
+
+    @Test
+    void generateShortUrlEmpty() throws Exception {
+        mockMvc.perform(post("/api/shorturl")
+                .contentType(MediaType.APPLICATION_JSON)
+                .accept(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new ShortUrlDto.Req("")))
+        )
+                .andDo(print())
+                .andExpect(status().isBadRequest())
+                .andExpect(jsonPath("$.success", is(false)))
+                .andExpect(jsonPath("$.response").isEmpty())
+                .andExpect(jsonPath("$.error.message", is("Url must be not empty")));
     }
 }
